5015a6600d626986847e4224955135c3b0a95c8f
Natalya Bystrova <natali.bystrova@noveogroup.com>
2024-05-24 11:55:42 +0300

    Code review fixes

--- src/app/useResize.ts
+++ src/app/useResize.ts
@@ -1,6 +1,6 @@
 import { RefObject, useEffect, useState } from "react";
 
-export const useResize = (htmlElement?: RefObject<HTMLInputElement>) => {
+export const useResize = (htmlElement?: RefObject<HTMLElement>) => {
   const [size, setSize] = useState([0, 0]);
   const isElement = htmlElement && htmlElement.current;
   const handleSetSize = () => {
